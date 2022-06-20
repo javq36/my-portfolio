0657a92ace53a632b5b5b73219fea2cbdca23d33
Alexander Villamizar <33869219+javq36@users.noreply.github.com>
2022-06-19 22:39:19 -0500

    First pull request

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Tekbees",
+        description:"We workeded in a few projects called TryME, Prestadero and Unicus. TryMe it's a plataform to sell recipes created by influencers in USA, Prestadero it's a plataform to loan money, and Unicus a plataform to digital identity verifiers.",
+        tags:["React","Node.js","styledComponents", "Next.js", "MongoDb", "MySql", "Redux", "Vue.js" ],
+        
+        demo:"https://tekbees.com/",
+        github:"https://tekbees.com/"
+    },
+    {
+        id:2,
+        name:"Icancha",
+        description:"We worked in a project that help people to reserve Artificial turf in the city of Bogota. The project was called SARI, and it's a plataform to buy and sell artificial turf in Bogota.",
+        tags:["React","Css"],
+        
+        demo:"https://www.icancha.com/",
+        github:"https://www.icancha.com/"
+        
+    },
+    {
+        id:3,
+        name:"AUTOMARCOL",
+        description:"We work here creating an app to help workers in a few things, like: creating suport tickets, checking list of insurance companies that owe spare parts, helps clients to create PQR's.",
+        tags:["Next.js", "Redux Toolkit", "StyledComponents", "Node.js", "MongoDb"],
+        
+        demo:"https://automarcol.com/",
+        github:"https://automarcol.com/"
+    },
+    {
+        id:4,
+        name:"Labco",
+        description:"My first work as developer, we made a system to autoritze people to logging trees in the city of Cucuta. Also manage Websites using Wordpress.",
+        tags:["PHP","Laravel","MySql", "Wordpress"],
+        
+        demo:"https://labco.com.co/",
+        github:"https://labco.com.co/"
+       
+    },{
+        id:5,
+        name:"Viviendas y Valores",
+        description:"Here I made my intership, we made a system that helps RRHH to manage CV's of employees, it store the hole information of a employee in a SQL database.",
+        tags:["PHP","HTML","CSS","MySql"],
+        
+        demo:"https://www.viviendasyvalores.com.co/",
+        github:"https://www.viviendasyvalores.com.co/"
+        
+    },
+    {
+        id:6,
+        name:"Universidad Simón Bolivar",
+        description:"A project that help the university to manage teachers; it's a plataform that store degrees, study programs, courses, and teachers in a SQL database.",
+        tags:["HTML","PHP","css", "MySql"],
+        
+        demo:"https://www.unisimon.edu.co/cucuta/",
+        github:"https://www.unisimon.edu.co/cucuta/"
+        
+    },
+    
+  
+] 
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,107 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Facebook, Github, LinkedIn } from "../components/AllSvgs";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled(motion.span)`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
+
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://github.com/javq36" }}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </NavLink>
+      </motion.div>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.2 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{
+            pathname:
+              "https://www.linkedin.com/in/alexander-villamizar-quñonez-8287651a3",
+          }}
+        >
+          <LinkedIn
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </NavLink>
+      </motion.div>
+      <motion.div
+        initial={{ transform: "scale(0)" }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://web.facebook.com/alexander.villamizar.37/" }}
+        >
+          <Facebook
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </NavLink>
+      </motion.div>
+
+      <Line
+        color={props.theme}
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1,
+          delay: 0.8,
+        }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
